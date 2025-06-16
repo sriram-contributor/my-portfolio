719485bbff7874c5f6baa8099756343a888f7bf9
sriram-contributor <200556815+sriram-contributor@users.noreply.github.com>
2025-06-16 11:05:59 +0530

    fix model name in app.py file

--- churn_app/app.py
+++ churn_app/app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 # Load model and scaler
-model = joblib.load('model.pkl')
+model = joblib.load('churn_model.pkl')
 scaler = joblib.load('scaler.pkl')
 
 st.title("📊 Customer Churn Predictor")
